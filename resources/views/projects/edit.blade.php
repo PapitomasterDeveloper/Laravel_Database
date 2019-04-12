265f056258f633082e18ec70102db4e2b1f737f1
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-04-12 00:14:28 -0500

    Created the logic of the Delete endpoint with his view

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,11 +1,14 @@
 @extends('layout')
 
 @section('content')
-	<h1 class="title">Edit Project page</h1>
+	<h1 class="title" style="margin-top: 1em;">Edit Project page</h1>
 
-	<form method="POST" action="/projects/{{ $project->id }}">
-		{{ method_field('PATCH') }}
-		{{ csrf_field() }}
+	<form method="POST" action="/projects/{{ $project->id }}" style="margin-bottom: 1em;">
+		<!-- {{ method_field('PATCH') }}-->
+		<!-- {{ csrf_field() }}-->
+    @method('DELETE')
+    @csrf
+
 
 		<div class="field">
 			<label class="label" for="title">Title</label>
@@ -28,5 +31,16 @@
 				<button type="submit" class="button is-link">Update Project</button>
 			</div>
 		</div>
+	</form>
+
+	<form method="POST" action="/projects/{{ $project->id }}">
+	{{ method_field('DELETE') }}
+	{{ csrf_field() }}
+
+		<div class="field">
+                        <div class="control">
+                                <button type="submit" class="button">Delete Project</button>
+                        </div>
+                </div>
 	</form>
 @endsection
